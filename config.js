17d6dc735526b64d0b1b6aa5844c0fd91d6f3d38
Seoth <7eot4.ha@gmail.com>
2026-08-23 14:24:27 +0200

    Activate booking backend and WhatsApp

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.KS_RENT_CONFIG = Object.freeze({
-  SUPABASE_URL: "",
-  SUPABASE_PUBLISHABLE_KEY: "",
-  WHATSAPP_NUMBER: "",
+  SUPABASE_URL: "https://hrudftrkqzhjyvgoyurb.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_hBPyP_xFyxMHEfK0ogCC6Q_lTCbJhFJ",
+  WHATSAPP_NUMBER: "48792830474",
   TURNSTILE_SITE_KEY: "",
 });
