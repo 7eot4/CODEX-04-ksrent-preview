ab830d1eb7397b80819504c32e10ff7259915293
Seoth <7eot4.ha@gmail.com>
2026-08-23 14:35:30 +0200

    Update WhatsApp contact number

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.KS_RENT_CONFIG = Object.freeze({
   SUPABASE_URL: "https://hrudftrkqzhjyvgoyurb.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_hBPyP_xFyxMHEfK0ogCC6Q_lTCbJhFJ",
-  WHATSAPP_NUMBER: "48792830474",
+  WHATSAPP_NUMBER: "48880313233",
   TURNSTILE_SITE_KEY: "",
 });
