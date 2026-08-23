514750fc108c193bd7edde0c30a8d7ed0d3562c2
Seoth <7eot4.ha@gmail.com>
2026-08-23 16:16:09 +0200

    Fix footer logo spacing and publish Gmail contact

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.MARE_LENTO_CONFIG = Object.freeze({
   SUPABASE_URL: "https://hrudftrkqzhjyvgoyurb.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_hBPyP_xFyxMHEfK0ogCC6Q_lTCbJhFJ",
   WHATSAPP_NUMBER: "48880313233",
-  CONTACT_EMAIL: "booking@marelento.it",
+  CONTACT_EMAIL: "marelento.booking@gmail.com",
   TURNSTILE_SITE_KEY: "",
 });
